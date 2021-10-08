3382abd61178de12c1b6a7a5dbc64bf37b87dae2
wmtowne <wmtowne@svsu.edu>
2021-10-08 08:02:06 -0400

    Working on menu functionality

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { animate, state, transition, trigger, style, query } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  selected:boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  clickHamburger(){
+    this.transformHamburger();
+    
+    if(this.selected){
+      this.showMenu();
+    }else{
+      this.hideMenu();
+    } 
+
+  }
+
+  transformHamburger(){
+    const bar1 = document.getElementsByClassName("header-hamburger-line1")[0] as HTMLElement;
+    const bar2 = document.getElementsByClassName("header-hamburger-line2")[0] as HTMLElement;
+    const bar3 = document.getElementsByClassName("header-hamburger-line3")[0] as HTMLElement;
+
+    if(bar1 && bar2 && bar3){
+      
+      if(this.selected){
+
+          this.selected = false;
+
+          bar1.style.transform = "rotate(0deg)";
+          bar2.style.opacity = "1";
+          bar3.style.transform = "rotate(0deg)";
+
+      }else{
+
+        this.selected = true;
+
+          bar1.style.transform = "rotate(-45deg) translateY(20px)";
+          bar2.style.opacity = "0";
+          bar3.style.transform = "rotate(45deg) translateY(-22px)";
+
+      }
+
+  }
+
+}
+
+showMenu(){
+  const menu = document.getElementsByClassName("menu-container")[0] as HTMLElement;
+
+  menu.style.right = "0px";
+}
+
+hideMenu(){
+  const menu = document.getElementsByClassName("menu-container")[0] as HTMLElement;
+
+  menu.style.right = "-300px";
+}
 
 }
--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { animate, state, transition, trigger, style, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
-  styleUrls: ['./project.component.css']
+  styleUrls: ['./project.component.css'],
+  animations: [
+    trigger('hideProject', [
+      state('refresh', style({opacity: '0'})),
+      transition('* => refresh', [
+        animate('1s', keyframes([
+          style({opacity: '1'}),
+          style({opacity: '0'})
+        ]))
+      ])
+    ]),
+    trigger('showProject', [
+      state('refresh2', style({opacity: '1'})),
+      transition('* => refresh2', [
+        animate('1s', keyframes([
+          style({opacity: '0'}),
+          style({opacity: '1'})
+        ]))
+      ])
+    ])
+  ]
 })
 export class ProjectComponent implements OnInit {
 
-  constructor() { }
+  state:string = "";
+
+  constructor(private cdr: ChangeDetectorRef) { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(){
+    this.state = "refresh";
+    this.cdr.detectChanges();
+  }
+
+  hideProject(){
+
+    var element = document.getElementById("test");
+
+    if(element){
+    element.textContent = "this is new";
+    }
+
+    this.state = "refresh2";
+
   }
 
 }
